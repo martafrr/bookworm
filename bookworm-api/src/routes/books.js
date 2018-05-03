13268cb7df7acfcd457aa6757d4e7a0bbc9da207
martafrr <martafarreribas@gmail.com>
2018-05-03 16:32:39 +0200

    server side auth and goodreads api

--- bookworm-api/src/routes/books.js
+++ bookworm-api/src/routes/books.js
@@ -1,26 +1,26 @@
 import express from 'express';
+import request from 'request-promise';
+import { parseString } from 'xml2js';
+import authenticate from '../middlewares/authenticate';
 
 const router = express.Router();
+router.use(authenticate);
 
 router.get('/search', (req, res) => {
-  res.json({
-    books: [
-      {
-        goodReadsId: 1,
-        title: "1984",
-        authors: "Orwell",
-        covers: ["", ""],
-        pages: 198
-      },
-      {
-        goodReadsId: 2,
-        title: "Three Men in a Boat",
-        authors: "Jerome K. Jerome",
-        covers: ["", ""],
-        pages: 256
-      }
-    ]
-  })
+  request.get(`https://www.goodreads.com/search/index.xml?key=${process.env
+    .GOODREADS_KEY}&q=${req.query.q}`
+  )
+  .then(result => 
+    parseString(result, (err, goodreadsResult) => 
+      res.json({ books: goodreadsResult.GoodreadsResponse.search[0].results[0].work.map(work => ({
+        goodReadsId: work.best_book[0].id[0]._,
+         title: work.best_book[0].title[0],
+         authors: work.best_book[0].author[0].name[0],
+         covers: [work.best_book[0].image_url[0]],
+      })) 
+    })
+    )
+  );
 });
 
 export default router;
